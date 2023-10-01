73fa1823011c6fb1773055eaaad113d5efad62f1
DhruvGarg007 <dhruvgargme@gmail.com>
2023-10-01 20:49:31 +0530

    Initial commitment

--- utils/contributors.js
+++ utils/contributors.js
@@ -24,6 +24,12 @@ const CONTRIBUTERS = [
         description: "Hello everyone, myself Shivansh from MnC Part 2",
         github: "https://github.com/shivansh-bhatnagar18"
     },
+    {
+        handle_name: 'DhruvGarg007',
+        name: "Dhruv Garg",
+        description: "Hello everyone, myself Dhruv Garg from EEE Part 1",
+        github: "https://github.com/DhruvGarg007"
+    }
     
 ];
 
